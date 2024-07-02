a901257ebe67517a399618230b69340718a9a57c
S-chahine <samah_chahine@outlook.com>
2024-07-02 18:01:15 -0400

    update URL

--- short-video-frontend/src/components/axios.js
+++ short-video-frontend/src/components/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || 'https://short-video-app.azurewebsites.net/',
+    baseURL: process.env.REACT_APP_BASE_URL || 'https://short-video-app.azurewebsites.net/v2/posts',
 })
 
 export default instance
